90f22e29fe179bb2e358d264fa694ecea647e86e
asc006-git <adhyatmasinghchauhan20031@gmail.com>
2026-06-01 16:03:48 +0530

    prisma: add intelligence summary fields to Summary model

---
+++ prisma/migrations/20260601090628_add_summary_intelligence_fields/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE "Summary" ADD COLUMN     "definitions" TEXT,
+ADD COLUMN     "detailedSummary" TEXT,
+ADD COLUMN     "facts" TEXT,
+ADD COLUMN     "formulas" TEXT,
+ADD COLUMN     "keyTakeaways" TEXT;
